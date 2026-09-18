6d3f6b86763808594fe9d3621266375ffbe4bd13
ZhannaCHMV <zh.chumachenko@rusbilling.com>
2026-09-18 11:50:15 +0000

    chore: module102

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,7 @@ COPY package.json .
 COPY package-lock.json .
 RUN npm install
 COPY . .
-
 RUN npm run build
-
 EXPOSE 3000
 # Start the SSR server
 CMD ["npm", "start"]
--- src/api/posts.js
+++ src/api/posts.js
@@ -12,17 +12,13 @@ export const getPostById = async (postId) => {
 }
 
 export const createPost = async (token, post) => {
-  const res = await fetch(
-    `${import.meta.env.VITE_BACKEND_URL}/
-        posts`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify(post),
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/posts`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
     },
-  )
+    body: JSON.stringify(post),
+  })
   return await res.json()
 }
